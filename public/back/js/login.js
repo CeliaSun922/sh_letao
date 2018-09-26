b437c02c8ef2ef468580a5f1d5095780e2027d58
CeliaSun922 <363921906@qq.com>
2018-09-26 19:05:23 +0800

    登录功能完成

---
+++ public/back/js/login.js
@@ -0,0 +1,69 @@
+;$(function(){
+  $("#form").bootstrapValidator({
+      feedbackIcons:{
+        valid: 'glyphicon glyphicon-ok',
+        invalid: 'glyphicon glyphicon-remove',
+        validating: 'glyphicon glyphicon-refresh'
+      },
+      fields:{
+        username:{
+          validators:{
+            notEmpty:{
+              message: "用户名不能为空"
+            },
+            stringLength:{
+              min: 2,
+              max: 6,
+              message: "用户名长度需要2-6位"
+            },
+            callback:{
+              message:"用户名错误"
+            }
+          }
+        },
+        password:{
+          validators:{
+            notEmpty:{
+              message:"密码不能为空"
+            },
+            stringLength:{
+              min: 6,
+              max: 12,
+              message: "密码长度为6-12位"
+            },
+            callback:{
+              message:"密码错误"
+            }
+          }
+        }
+      }
+
+  })
+
+  $("#form").on('success.form.bv', function(e){
+      e.preventDefault();
+      $.ajax({
+        type:"post",
+        url:"/employee/employeeLogin",
+        data: $("#form").serialize(),
+        dataType: "json",
+        success:function(info){
+          if(info.success){
+          location.href('login.html')
+        }
+          if(info.error == 1000){
+            $('#form').data('bootstrapValidator').updateStatus("username","INVALID","callback")
+          }
+          if(info.error == 1001){
+            $('#form').data('bootstrapValidator').updatestatus("password","INVALID","callback")
+          }
+        }
+      })
+    })
+
+    $('[type="reset"]').on('click',function(){
+      $('#form').data('bootstrapValidator').resetForm();
+    })
+  
+
+})()
