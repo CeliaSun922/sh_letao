1fcb4166d147e5a459b80fdb1580baa5f76af4ca
CeliaSun922 <363921906@qq.com>
2018-09-27 18:09:18 +0800

    登录页功能完成

---
+++ public/back/js/comment.js
@@ -0,0 +1,14 @@
+// 进度条功能
+
+  $(document).ajaxStart(function(){
+    NProgress.start();
+
+  });
+
+  $(document).ajaxStop(function(){
+    setTimeout(function(){
+      NProgress.done();
+
+    },500);
+  })
+
--- public/back/js/login.js
+++ public/back/js/login.js
@@ -17,7 +17,7 @@
               message: "用户名长度需要2-6位"
             },
             callback:{
-              message:"用户名错误"
+              message:"用户名不存在"
             }
           }
         },
@@ -49,12 +49,12 @@
         dataType: "json",
         success:function(info){
           if(info.success){
-          location.href('login.html')
+          location.href='index.html';
         }
-          if(info.error == 1000){
+          if(info.error === 1000){
             $('#form').data('bootstrapValidator').updateStatus("username","INVALID","callback")
           }
-          if(info.error == 1001){
+          if(info.error === 1001){
             $('#form').data('bootstrapValidator').updatestatus("password","INVALID","callback")
           }
         }
@@ -66,4 +66,4 @@
     })
   
 
-})()
+})
