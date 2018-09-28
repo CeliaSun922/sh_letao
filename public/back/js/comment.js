21859aa5c45d14d6d9542aef29b977d0bee20650
CeliaSun922 <363921906@qq.com>
2018-09-28 18:08:03 +0800

    首页功能完成

--- public/back/js/comment.js
+++ public/back/js/comment.js
@@ -12,3 +12,45 @@
     },500);
   })
 
+  // 侧边栏分类栏显示隐藏切换
+  $('.side_nav .main_category').click(function(){
+    $('.side_nav .side_categray').stop().slideToggle()
+
+  })
+
+  //侧边栏显示隐藏切换
+
+  $('.lt_topnav .nav_menu').click(function(){
+
+    $('.lt_side').toggleClass('hidemenu');
+    $('.lt_topnav').toggleClass('hidemenu');
+    $('.lt_main').toggleClass('hidemenu');
+
+
+  })
+
+  //退出时显示模态框
+
+  $('.lt_topnav .nav_checkout').click(function(){
+    $('#myModal').modal('show');
+
+  })
+
+  //退出功能转到Login页面
+
+  $('#modle_checkout').click(function(){
+    $.ajax({
+      type:"get",
+      url:"/employee/employeeLogout",
+      dataType:"json",
+      success:function( info ){
+        if( info.success ){
+          location.href = "login.html";
+        }
+      }
+
+
+    })
+
+
+  })
