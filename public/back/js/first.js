337d71bcd39d391571936be6cbc9caac2a52951a
CeliaSun922 <363921906@qq.com>
2018-09-29 21:13:23 +0800

    一级分类完成

--- public/back/js/first.js
+++ public/back/js/first.js
@@ -34,6 +34,61 @@ $(function(){
           }
         });
       }
-    })
+    });
   }
-})
+
+        $("#addCategory").on("click", function(){
+          $("#addModal").modal("show");
+          
+
+        })
+        $("#form").bootstrapValidator({
+          //小图标
+          feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+          },
+          //校验规则
+          fields:{
+            categoryName:{
+        
+              validators:{
+                //非空
+                notEmpty:{
+                  message:"请输入一级分类"
+                }
+              }
+        
+            }
+          }
+        });
+        $("#form").on("success.form.bv",function(e){
+          e.preventDefault();
+
+            $.ajax({
+              type:"post",
+              url:"/category/addTopCategory",
+              data: $('#form').serialize(),
+              dataType:"json",
+              success: function(info){
+                console.log(info);
+                if(info.success){
+                $('#addModal').modal('hide');
+                currentPage = 1;
+                render();
+                $('#form').data("bootstrapValidator").resetForm(true);
+              }
+            }
+
+            })
+
+
+
+        });
+
+
+
+      
+    })
+  
